6b7bbca52125a7edaf1c64f103fec8e4f4c1f834
springboot <nhokxuanquy@gmail.com>
2022-09-24 10:37:03 +0700

    up

--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -5,37 +5,7 @@ import './css.css'
 function HomePage() {
     return (
         <>
-            <div className="flip-animation">
-                <span>C</span>
-                <span>h</span>
-                <span>a</span>
-                <span>o</span>
-                <span>s</span>
-                <span>.</span>
-            </div>
-
-            <div className="typed-animation">
-                <h1 className="typed-out">I'm Rang Farm</h1>
-            </div>
-
-            <div className="typed-animation">
-                <h1 className="typed-out">I'm Rang Farm</h1>
-            </div>
-
-
-            <div class="music-waves-2">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-
-            <Link to={'admin/index'} className="button">Back dashboard</Link>
-            <NavLink to={'login'} className="button">Sign in</NavLink>
-            <NavLink to={'register'} className="button">Sign up</NavLink>
+           <h2>okkkkkkkkkkkk</h2>
 
         </>
 
