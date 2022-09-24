6caa981aeebe16cc76fac628b61f1c495b950956
gp <tranhrp@gmail.com>
2022-09-24 18:43:11 +0700

    cc

--- src/views/pillar/Pillar.jsx
+++ src/views/pillar/Pillar.jsx
@@ -10,7 +10,13 @@ import TableRow from '@mui/material/TableRow';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
-import { Button } from '@mui/material';
+import { Button, IconButton, InputBase } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SearchIcon from '@mui/icons-material/Search';
+import EditIcon from '@mui/icons-material/Edit';
+
+import { Container, DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown } from 'reactstrap';
+
 const columns = [
     { id: 'name', label: 'Name', minWidth: 170 },
     { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
@@ -28,25 +34,7 @@ const columns = [
         align: 'right',
         format: (value) => value.toLocaleString('en-US'),
     },
-    {
-        id: 'update',
-        label: 'update',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toFixed(2),
-    },
-    {
-        id: 'delete',
-        label: 'delete',
-        minWidth: 170,
-        align: 'right',
-    },
 ];
-
-function createData(name, code, population, size, update) {
-    const density = population / size;
-    return { name, code, population, size, density, update };
-}
 
 const rows = [
     {
@@ -79,12 +67,40 @@ const rows = [
         density: "density"
 
     },
-    // createData('India', 'IN', 1324171354, 3287263, <Button variant="contained">Contained</Button>),
-    // createData('China', 'CN', 1403500365, 9596961, <Button variant="contained">Contained</Button>),
-    // createData('Italy', 'IT', 60483973, 301340, <Button variant="contained">Contained</Button>),
-    // createData('India', 'IN', 1324171354, 3287263, <Button variant="contained">Contained</Button>),
-];
+    {
+        name: 'Population',
+        code: 'IN',
+        population: 12321,
+        size: 123,
+        density: "density"
 
+    },
+    {
+        name: 'Population',
+        code: 'IN',
+        population: 12321,
+        size: 123,
+        density: "density"
+
+    },
+    {
+        name: 'Population',
+        code: 'IN',
+        population: 12321,
+        size: 123,
+        density: "density"
+
+    },
+    {
+        name: 'Population',
+        code: 'IN',
+        population: 12321,
+        size: 123,
+        density: "density"
+
+    },
+
+];
 export default function Pillar() {
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -100,17 +116,33 @@ export default function Pillar() {
     return (
         <>
         
-            <div style={{ height: "200px" }} className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+        <Container fluid style={{ height: "200px" }} className="header bg-gradient-info pb-8 pt-5 pt-md-8 ">
+                <Paper sx={{ width: '100%', overflow: 'hidden', padding: '10px' }}>
 
-                <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-                    <TextField sx={{ mt: "7px", width: "400px" }} id="outlined-basic" label="Search" variant="outlined" />
+                    <div style={{ width: '100%', display: "flex",flexDirection: "row"}}>
+                        <Button sx={{padding:"10px 5px", marginRight: '2%', height: '3.2em', width:"15%"}} variant="contained" color="success">
+                            Add customer
+                        </Button>
+                        <Paper sx={{ boxShadow: "none", border: "1px solid #ddd", display: 'flex', padding: '7px 7px 3px 7px', width: '100%', marginBottom: '20px', borderRadius: '7px' }}>
+                            <IconButton type="button" sx={{ p: '0px', }} aria-label="search">
+                                <SearchIcon />
+                            </IconButton>
+                            <InputBase
+                                sx={{ ml: 1, flex: 1, width: '90%', fontSize: '1.1em' }}
+                                placeholder="Search Name Customer"
+                            />
+                        </Paper>
+                    </div>
 
-                    <TableContainer sx={{ maxHeight: 505 }}>
-                        <Table stickyHeader aria-label="sticky table">
+                    {/* <TextField sx={{ mt: "7px", width: "400px" }} id="outlined-basic" label="Search" variant="outlined" /> */}
+                    {/* stickyHeader */}
+                    <TableContainer sx={{ minHeight: '29em' }}>
+                        <Table aria-label="sticky table">
                             <TableHead>
                                 <TableRow>
                                     {columns.map((column) => (
                                         <TableCell
+                                            sx={{ color: 'black', fontWeight: '600', fontSize: '1em' }}
                                             key={column.id}
                                             align={column.align}
                                             style={{ minWidth: column.minWidth }}
@@ -129,15 +161,41 @@ export default function Pillar() {
                                             <TableCell > {item.code} </TableCell>
                                             <TableCell sx={{ textAlign: 'right' }}>  {item.density} </TableCell>
                                             <TableCell sx={{ textAlign: 'right' }}> {item.size}</TableCell>
-                                            <TableCell sx={{ textAlign: 'right' }}><Button variant="contained">update</Button></TableCell>
-                                            <TableCell sx={{ textAlign: 'right' }}><Button variant="contained">delete</Button></TableCell>
+                                            <TableCell>
+                                                <UncontrolledDropdown>
+                                                    <DropdownToggle
+                                                        className="btn-icon-only text-light"
+                                                        href="#pablo"
+                                                        role="button"
+                                                        size="sm"
+                                                        color=""
+                                                        onClick={(e) => e.preventDefault()}
+                                                    >
+                                                        <i className="fas fa-ellipsis-v" />
+                                                    </DropdownToggle>
+                                                    <DropdownMenu className="dropdown-menu-arrow" right>
+                                                        <DropdownItem
+                                                            href="#pablo"
+                                                            onClick={(e) => e.preventDefault()}>
+                                                            <DeleteIcon></DeleteIcon>
+                                                            Delete
+                                                        </DropdownItem>
+                                                        <DropdownItem
+                                                            href="#pablo"
+                                                            onClick={(e) => e.preventDefault()}>
+                                                            <EditIcon></EditIcon>
+                                                            Update
+                                                        </DropdownItem>
+                                                    </DropdownMenu>
+                                                </UncontrolledDropdown>
+                                            </TableCell>
                                         </TableRow>
                                     ))}
                             </TableBody>
                         </Table>
                     </TableContainer>
                     <TablePagination
-                        rowsPerPageOptions={[10, 25, 100]}
+                        rowsPerPageOptions={[6, 10, 25, 100]}
                         component="div"
                         count={rows.length}
                         rowsPerPage={rowsPerPage}
@@ -146,7 +204,7 @@ export default function Pillar() {
                         onRowsPerPageChange={handleChangeRowsPerPage}
                     />
                 </Paper>
-            </div>
+            </Container>
         </>
     )
 }
