425f029c27c0bd601846b0fca1221a3448b6ecde
springboot <nhokxuanquy@gmail.com>
2022-09-25 11:49:55 +0700

    edit api

--- src/utils/const.js
+++ src/utils/const.js
@@ -4,12 +4,14 @@ export const API_SIGNUP = `${API}/signup`;
 
 export const API_SIGNIN = `${API}/login`;
 
-export const API_GET_PILLAR = `${API}/address/`;
+export const API_GET_PILLAR = `${API}/admin/address/`;
 
-export const API_ADD_PILLAR = `${API}/address/add`;
+export const API_ADD_PILLAR = `${API}/admin/address/add`;
 
-export const API_DELETE_PILLAR = `${API}/address/delete?id=`;
+export const API_DELETE_PILLAR = `${API}/admin/address/delete?id=`;
 
-export const API_EDIT_PILLAR = `${API}/address/edit`;
+export const API_EDIT_PILLAR = `${API}/admin/address/edit`;
+
+
 
 
