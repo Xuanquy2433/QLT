e84cbbf9232aecb449c6e75d51b7552243bc0729
gp <tranhrp@gmail.com>
2022-10-08 12:22:38 +0700

    file address

--- src/views/pillarAddress/CreateAddress.jsx
+++ src/views/pillarAddress/CreateAddress.jsx
@@ -3,15 +3,26 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import FilledInput from '@mui/material/FilledInput';
+
 export default function CreatePillar({ data, onSubmit, open, setOpen }) {
 
     const [dataAddress, setDataAddress] = useState({
         city: '',
         street: '',
         description: '',
+        multipartFile: ''
     })
+    const [selectedImage, setSelectedImage] = useState(null);
+
     const onChangeText = (event) => {
         setDataAddress({ ...dataAddress, [event.target.name]: event.target.value })
+    }
+
+    const onChangeImage = (event) => {
+        const value = event.target.files[0]
+        setSelectedImage(event.target.files[0])
+        setDataAddress({ ...dataAddress, multipartFile: (value) })
     }
 
     const onClickAdd = (event) => {
@@ -21,6 +32,7 @@ export default function CreatePillar({ data, onSubmit, open, setOpen }) {
 
     const handleClose = () => setOpen(false);
 
+    console.log(dataAddress);
     return (
         <div>
             <Modal
@@ -38,13 +50,23 @@ export default function CreatePillar({ data, onSubmit, open, setOpen }) {
                         padding: '10px'
                     }}
                 >
+                    <div>
+                        {selectedImage && (
+                            <div>
+                                <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
+                                <br />
+                                <button onClick={() => setSelectedImage(null)}>Remove</button>
+                            </div>
+                        )}
+
+                    </div>
                     <h2 style={{ textAlign: 'center' }}>ADD ADDRESS</h2>
                     <div style={{ display: 'flex', flexDirection: "column-reverse", margin: "10px" }} className="form-flex">
                         <TextField onChange={onChangeText} defaultValue='' name="description" style={{ margin: '5px' }} fullWidth label='Chú thích' />
 
                         <TextField onChange={onChangeText} defaultValue='' name="street" style={{ margin: '5px' }} fullWidth label='Đường' />
                         <TextField onChange={onChangeText} defaultValue='' name="city" style={{ margin: '5px' }} fullWidth label='Thành phố' />
-
+                        <TextField onChange={onChangeImage} style={{ margin: '5px -5px 5px 5px' }} name="multipartFile" type="file" />
                     </div>
                     <div style={{ display: "flex", justifyContent: "center" }}>
                         <Button sx={{ marginRight: "5px" }} onClick={handleClose} variant="contained" color="success">
