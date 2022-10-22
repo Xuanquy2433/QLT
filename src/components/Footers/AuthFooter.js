0b574e93c7da5be3c8aff5c1f6bfe553466cbef4
springboot <nhokxuanquy@gmail.com>
2022-10-22 16:44:51 +0700

    update footer

--- src/components/Footers/AuthFooter.js
+++ src/components/Footers/AuthFooter.js
@@ -9,7 +9,7 @@ const Login = () => {
         {/* Footer */}
         <footer
           className="text-center text-lg-start text-white"
-          style={{ backgroundColor: "#1c2331",marginTop: '50px' }}
+          style={{ backgroundColor: "#1c2331", marginTop: '50px' }}
         >
           {/* Section: Social media */}
           <section
@@ -53,44 +53,43 @@ const Login = () => {
                 {/* Grid column */}
                 <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                   {/* Content */}
-                  <h6 style={{ fontSize: '1.2em', color: 'white', fontWeight: '600' }} className="text-uppercase fw-bold">Company name</h6>
+                  <h6 style={{ fontSize: '1.2em', color: 'white', fontWeight: '600' }} className="text-uppercase fw-bold">TRUYỀN THÔNG ACN</h6>
                   <hr
                     className="mb-4 mt-0 d-inline-block mx-auto"
                     style={{ width: 60, backgroundColor: "#7c4dff", height: 2 }}
                   />
                   <p>
-                    Here you can use rows and columns to organize your footer
-                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit.
+                    Chúng tôi là TRUYỀN THÔNG ACN sẽ giúp bạn cung cấp các giải pháp công nghệ thông tin phù hợp nhất
+                    nhằm giúp cho hoạt động kinh doanh của doanh nghiệp đạt được hiệu quả.
                   </p>
                 </div>
                 {/* Grid column */}
                 {/* Grid column */}
                 <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
                   {/* Links */}
-                  <h6 style={{ fontSize: '1.2em', color: 'white', fontWeight: '600' }} className="text-uppercase fw-bold">Products</h6>
+                  <h6 style={{ fontSize: '1.2em', color: 'white', fontWeight: '600' }} className="text-uppercase fw-bold">Dịch vụ</h6>
                   <hr
                     className="mb-4 mt-0 d-inline-block mx-auto"
                     style={{ width: 60, backgroundColor: "#7c4dff", height: 2 }}
                   />
                   <p>
                     <NavLink to={''} className="text-white">
-                      React js
+                      Thiết kế website
                     </NavLink>
                   </p>
                   <p>
                     <NavLink to={''} className="text-white">
-                      Spring boot
+                      Thiết kế phần mềm
                     </NavLink>
                   </p>
                   <p>
                     <NavLink to={''} className="text-white">
-                      ReactStrap
+                      Thiết kế landing page
                     </NavLink>
                   </p>
                   <p>
                     <NavLink to={''} className="text-white">
-                      Rest API
+                      Thiết kế cơ sở dữ liệu
                     </NavLink>
                   </p>
                 </div>
@@ -98,29 +97,24 @@ const Login = () => {
                 {/* Grid column */}
                 <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
                   {/* Links */}
-                  <h6 style={{ fontSize: '1.2em', color: 'white', fontWeight: '600' }} className="text-uppercase fw-bold">Useful links</h6>
+                  <h6 style={{ fontSize: '1.2em', color: 'white', fontWeight: '600' }} className="text-uppercase fw-bold">Thiết kế website</h6>
                   <hr
                     className="mb-4 mt-0 d-inline-block mx-auto"
                     style={{ width: 60, backgroundColor: "#7c4dff", height: 2 }}
                   />
                   <p>
                     <a href="#!" className="text-white">
-                      Your Account
+                      Thiết kế website bán hàng
                     </a>
                   </p>
                   <p>
                     <a href="#!" className="text-white">
-                      Become an Affiliate
+                      Thiết kế web thời trang
                     </a>
                   </p>
                   <p>
                     <a href="#!" className="text-white">
-                      Shipping Rates
-                    </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-white">
-                      Help
+                      Thiết kế web thương mại điện tử
                     </a>
                   </p>
                 </div>
@@ -128,23 +122,23 @@ const Login = () => {
                 {/* Grid column */}
                 <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                   {/* Links */}
-                  <h6 style={{ fontSize: '1.2em', color: 'white', fontWeight: '600' }} className="text-uppercase fw-bold">Contact</h6>
+                  <h6 style={{ fontSize: '1.2em', color: 'white', fontWeight: '600' }} className="text-uppercase fw-bold">Kết nối</h6>
                   <hr
                     className="mb-4 mt-0 d-inline-block mx-auto"
                     style={{ width: 60, backgroundColor: "#7c4dff", height: 2 }}
                   />
                   <p>
-                    <i className="fas fa-home mr-3" /> BMT , {new Date().getFullYear()}{" "} , VN
+                    <i className="fas fa-home mr-3" />Tầng 2 Toà nhà 49 Nguyễn Trãi, TP. Buôn Ma Thuột
                   </p>
                   <p>
-                    <i className="fas fa-envelope mr-3" /> info@example.com
+                    <i className="fas fa-envelope mr-3" />  truyenthongacn@gmail.com
                   </p>
                   <p>
-                    <i className="fas fa-phone mr-3" /> + 01 234 567 88
+                    <i className="fas fa-phone mr-3" /> Hotline: 090 518 48 71
                   </p>
-                  <p>
+                  {/* <p>
                     <i className="fas fa-print mr-3" /> + 01 234 567 89
-                  </p>
+                  </p> */}
                 </div>
                 {/* Grid column */}
               </div>
