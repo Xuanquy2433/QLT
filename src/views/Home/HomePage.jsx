c31c03d4f4cbbdd702f430aff583a3a5582d3700
gp <tranhrp@gmail.com>
2022-10-24 21:38:01 +0700

    phone

--- src/views/Home/HomePage.jsx
+++ src/views/Home/HomePage.jsx
@@ -23,13 +23,29 @@ function HomePage() {
             <BannerSlide />
             <hr />
             <div>
-                <About/>
+                <About />
             </div>
-           <div>
-           <TableAddress />
-           </div>
+            <div>
+                <TableAddress />
+            </div>
             <hr />
-            
+            <div className="hotline-phone-ring-wrap">
+                <div className="hotline-phone-ring">
+                    <div className="hotline-phone-ring-circle" />
+                    <div className="hotline-phone-ring-circle-fill" />
+                    <div className="hotline-phone-ring-img-circle">
+                        <a href="tel:0987654321" className="pps-btn-img">
+                            <img src="https://nguyenhung.net/wp-content/uploads/2019/05/icon-call-nh.png" alt="Gọi điện thoại" width={50} />
+                        </a>
+                    </div>
+                </div>
+                <div className="hotline-bar">
+                    <a href="tel:0987654321">
+                        <span className="text-hotline">0987.654.321</span>
+                    </a>
+                </div>
+            </div>
+
             {/* <TableProduct /> */}
 
         </div>
