67f2b3e1b1d177468b32982cd6856c33f6145c56
springboot <nhokxuanquy@gmail.com>
2022-10-27 19:03:17 +0700

    test

--- src/components/Navbars/Notification.jsx
+++ src/components/Navbars/Notification.jsx
@@ -25,8 +25,9 @@ function Notification() {
     const [open, setOpen] = React.useState(false);
     const anchorRef = React.useRef(null);
     const handleToggle = () => {
-        // setOpen((prevOpen) => !prevOpen);
         fetchAPI()
+        // setOpen((prevOpen) => !prevOpen);
+        setOpen(true)
     };
 
     const handleClose = (event) => {
@@ -77,7 +78,7 @@ function Notification() {
                         <NotificationsNoneIcon />
                     </p>
                     <Popper
-                        open={true}
+                        open={open}
                         anchorEl={anchorRef.current}
                         role={undefined}
                         placement="bottom-start"
