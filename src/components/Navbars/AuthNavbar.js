ff5e396e1fff65a3df05003ce9b56901e2e736f9
Trung Quý <letrungquy50@gmail.com>
2022-10-27 15:43:53 +0700

    notification

--- src/components/Navbars/AuthNavbar.js
+++ src/components/Navbars/AuthNavbar.js
@@ -21,7 +21,20 @@ import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import FolderSharedOutlinedIcon from '@mui/icons-material/FolderSharedOutlined'; import './style.css'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import React from 'react';
-import ChatRoom from 'views/Realtime/ChatRoom';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+
+import Button from '@mui/material/Button';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import Stack from '@mui/material/Stack';
+
+
+
+
 
 const AdminNavbar = () => {
   let decoded;
@@ -59,6 +72,43 @@ const AdminNavbar = () => {
     fontSize: "0.9rem",
     fontWeight: "400"
   }
+
+
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === 'Escape') {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+
   return (
     <>
       <Navbar className="navbar-top navbar-horizontal navbar-dark" expand="md">
@@ -170,8 +220,75 @@ const AdminNavbar = () => {
 
               <NavItem className="menu-hover nav-link-icon">
                 <NavLink className="nav-link-icon" tag={Link}>
-                  <i className="ni ni-notification-70" />
-                  <span className="nav-link-inner--text"><ChatRoom />Thông báo </span>
+                  {/* <i className="ni ni-notification-70" />
+
+                  <button>
+                    <span className="nav-link-inner--text"><ChatRoom />Thông báo </span>
+                  </button>
+                  <ul className='menu'>
+                    <li className='menu-item'>
+                      <a href="#" >
+                        <div >You have <b>3 new themes</b> trending</div>
+                        <div >15 minutes ago</div>
+                      </a>
+                    </li>
+                  </ul> */}
+                  <Stack  direction="row" spacing={2}>
+                    <div className='menu-lv2'>
+                    
+                      <Button
+                        
+                        ref={anchorRef}
+                        id="composition-button"
+                        aria-controls={open ? 'composition-menu' : undefined}
+                        aria-expanded={open ? 'true' : undefined}
+                        aria-haspopup="true"
+                        onClick={handleToggle}
+                      >
+                        <NotificationsNoneIcon />
+                      </Button>
+                      <Popper
+                        open={open}
+                        anchorEl={anchorRef.current}
+                        role={undefined}
+                        placement="bottom-start"
+                        transition
+                        disablePortal
+                      >
+                        {({ TransitionProps, placement }) => (
+                          <Grow
+                            {...TransitionProps}
+                            
+                          >
+                            <Paper>
+                              <ClickAwayListener onClickAway={handleClose}>
+                                <MenuList
+                                  autoFocusItem={open}
+                                  id="composition-menu"
+                                  aria-labelledby="composition-button"
+                                  onKeyDown={handleListKeyDown}
+                                >
+                                  <MenuItem onClick={handleClose}>
+                                    <div className='notification' >You have <b>3 new themes</b> trending</div>
+                                    <div className='notification-time' >15 minutes ago</div>
+
+                                  </MenuItem>
+                                  <MenuItem onClick={handleClose}>
+                                    <div className='notification' >You have <b>4 new themes</b> trending</div>
+                                    <div className='notification-time' >10 minutes ago</div>
+
+                                  </MenuItem>
+                                
+
+                               
+                                </MenuList>
+                              </ClickAwayListener>
+                            </Paper>
+                          </Grow>
+                        )}
+                      </Popper>
+                    </div>
+                  </Stack>
                 </NavLink>
               </NavItem>
               {token && decoded ?
