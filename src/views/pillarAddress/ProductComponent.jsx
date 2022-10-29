d5344f7902f42c68866b2b0f19417a7eb1d351b4
springboot <nhokxuanquy@gmail.com>
2022-10-29 16:47:52 +0700

    pagination in home

--- src/views/pillarAddress/ProductComponent.jsx
+++ src/views/pillarAddress/ProductComponent.jsx
@@ -1,18 +1,22 @@
 import Countdown from "react-countdown";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
-import React from "react";
+import React, { useState } from "react";
 import { toast } from 'react-toastify';
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 import { API_ADD_CART } from "utils/const";
-import { Button } from "@mui/material";
+import { Button, createTheme, Pagination, Stack } from "@mui/material";
 import { formatMoney } from "common/formatMoney";
 import { API_ADD_CART_PRE_ORDER } from "utils/const";
 import Moment from "react-moment";
 import { AiOutlineHeart } from "react-icons/ai";
 import './ProductComponent.css'
 import { API_WISHLIST_ADD } from "utils/const";
+import usePagination from "./Pagination";
+import './css.css'
+import { ThemeProvider } from "styled-components";
+
 function ProductComponent({ product }) {
 
   const renderer = ({ hours, minutes, completed }) => {
@@ -203,67 +207,95 @@ function ProductComponent({ product }) {
     }
 
   }
+
+  let [page, setPage] = useState(1);
+  const PER_PAGE = 8;
+
+  const count = Math.ceil(product.length / PER_PAGE);
+  const _DATA = usePagination(product, PER_PAGE);
+
+  // const count = 10
+  // const _DATA = usePagination(product, PER_PAGE);
+
+  const handleChange = (e, p) => {
+    setPage(p);
+    _DATA.jump(p);
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#00ADB5',
+        contrastText: '#EEE'
+      },
+      secondary: {
+        main: '#EEE'
+      },
+      // background: 'none'
+    },
+    typography: {
+      fontFamily: [
+        'Poppins'
+      ],
+      fontSize: 18,
+    }
+  })
+
   return (
-    <div style={{
-      display: "flex", width: '1300px', flexWrap: "wrap", justifyContent: "center"
-      , marginTop: '50px', marginBottom: '150px'
-    }}>
-      {
-        product.map((item, index) => (
-          <div key={index} style={{ flexDirection: "column", float: "left", position: 'relative', backgroundColor: "#FFFFFF", marginTop: '20px', width: "23%", margin: "5px", display: "flex", padding: "10px", borderRadius: "15px", }}>
-            <div style={{ width: "100%", display: "flex", flexDirection: "column", justifyContent: "center" }}>
-              <img style={{ width: '100%', height: "27vh", borderRadius: "8px" }} src={item.photosImagePath} alt="" />
-            </div>
-            <div style={{ width: "100%", textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <div style={{
+          display: "flex", width: '1300px', flexWrap: "wrap", justifyContent: "center"
+          , marginTop: '50px', marginBottom: '150px'
+        }}>
+          {
+            _DATA.currentData().map((item, index) => (
+              <div key={index} style={{ flexDirection: "column", float: "left", position: 'relative', backgroundColor: "#FFFFFF", marginTop: '20px', width: "23%", margin: "5px", display: "flex", padding: "10px", borderRadius: "15px", }}>
+                <div style={{ width: "100%", display: "flex", flexDirection: "column", justifyContent: "center" }}>
+                  <img style={{ width: '100%', height: "27vh", borderRadius: "8px" }} src={item.photosImagePath} alt="" />
+                </div>
+                <div style={{ width: "100%", textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
 
-              <h1 style={{ fontSize: "28px", fontWeight: "600", marginBottom: '1px', color: "#444444" }}> {item.name}</h1>
-              <h2 style={{ color: '#D70018' }}> {formatMoney(item.price)} VNĐ</h2>
-              <h3>Loại trụ: {item.category.name}</h3>
-              <h4> {item.description}</h4>
-              {item.status === 'AVAILABLE' ?
-                <Button className="btn-cart-cus" style={{
-                  '&:hover': { backgroundColor: "#5372E4" }, fontWeight: "500", width: "100%",
-                  border: "1px solid #5372E4", background: "#5372E4", color: "#FFFFFF",
-                }} onClick={(e) => addCart({ ...item })} variant="contained" color="success">
-                  Thêm vào giỏ
-                </Button> :
-                <Button style={{
-                  '&:hover': { backgroundColor: "#5372E4" }, fontWeight: "500", width: "100%"
-                  , border: "1px solid #5372E4", background: "none", color: "red", boxShadow: "none", backgroundColor: 'rgb(255, 210, 210)'
-                }} disabled variant="contained" >
-                  Đã cho thuê
-                </Button>}
-              {item.expiredDate !== null ? <h4 style={{ marginTop: '15px' }}> Ngày hết hạn: <span style={{ color: 'red' }}> <Moment format="DD/MM/YYYY">{item.expiredDate}</Moment></span> </h4> : ''}
-            </div>
-            {
-              item.preOrdered === false ?
-                <Button sx={{ height: '9vh', fontSize: '0.6em', width: '10%', position: 'absolute', top: '0', right: '0', backgroundColor: ' #F4364C' }}
-                  onClick={(e) => addCartPreOrder({ ...item })} variant="contained" >
-                  Đặt trước
-                </Button> : ''
-            }
-            < div style={{ display: "flex", alignItems: "center", marginTop: "5px", justifyContent: "end" }}>
-              Yêu thích  <AiOutlineHeart onClick={(e) => onClickAddWishList(item.id)} className="colorHeart-cus" style={{ fontSize: "30px", color: "rgb(215,0,24)", cursor: "pointer" }} />
-            </div>
-          </div >
-        ))
-      }</div >
-    // <TableRow hover role="checkbox" >
-    //   <TableCell sx={{}}>
-    //     <img style={{ width: '80px', borderRadius: "8px" }} src={product.photosImagePath} alt="" />
-    //   </TableCell>
-    //   <TableCell sx={{ textAlign: 'center' }}>{product.name}</TableCell>
-    //   <TableCell sx={{ textAlign: 'center' }}>{product.category.name}</TableCell>
-    //   <TableCell sx={{ textAlign: 'center' }}>{product.price}</TableCell>
-
-    //   <TableCell sx={{ textAlign: 'right' }}>
-    //     <Countdown
-    //       date={product.expiredDate}
-    //       renderer={renderer}
-    //     ></Countdown>
-    //     {buttons(product.isPreOrdered, product.status === "AVAILABLE")}
-    //   </TableCell>
-    // </TableRow>
+                  <h1 style={{ fontSize: "28px", fontWeight: "600", marginBottom: '1px', color: "#444444" }}> {item.name}</h1>
+                  <h2 style={{ color: '#D70018' }}> {formatMoney(item.price)} VNĐ</h2>
+                  <h3>Loại trụ: {item.category.name}</h3>
+                  <h4> {item.description}</h4>
+                  {item.status === 'AVAILABLE' ?
+                    <Button className="btn-cart-cus" style={{
+                      '&:hover': { backgroundColor: "#5372E4" }, fontWeight: "500", width: "100%",
+                      border: "1px solid #5372E4", background: "#5372E4", color: "#FFFFFF",
+                    }} onClick={(e) => addCart({ ...item })} variant="contained" color="success">
+                      Thêm vào giỏ
+                    </Button> :
+                    <Button style={{
+                      '&:hover': { backgroundColor: "#5372E4" }, fontWeight: "500", width: "100%"
+                      , border: "1px solid #5372E4", background: "none", color: "red", boxShadow: "none", backgroundColor: 'rgb(255, 210, 210)'
+                    }} disabled variant="contained" >
+                      Đã cho thuê
+                    </Button>}
+                  {item.expiredDate !== null ? <h4 style={{ marginTop: '15px' }}> Ngày hết hạn: <span style={{ color: 'red' }}> <Moment format="DD/MM/YYYY">{item.expiredDate}</Moment></span> </h4> : ''}
+                </div>
+                {
+                  item.preOrdered === false ?
+                    <Button sx={{ height: '9vh', fontSize: '0.6em', width: '10%', position: 'absolute', top: '0', right: '0', backgroundColor: ' #F4364C' }}
+                      onClick={(e) => addCartPreOrder({ ...item })} variant="contained" >
+                      Đặt trước
+                    </Button> : ''
+                }
+                < div style={{ display: "flex", alignItems: "center", marginTop: "5px", justifyContent: "end" }}>
+                  Yêu thích  <AiOutlineHeart onClick={(e) => onClickAddWishList(item.id)} className="colorHeart-cus" style={{ fontSize: "30px", color: "rgb(215,0,24)", cursor: "pointer" }} />
+                </div>
+              </div >
+            ))
+          }
+        </div >
+        <Stack alignItems="center">
+          <Pagination
+            sx={{ button: { color: '#ffffff', width: '100%', margin: 'auto' } }}
+            count={count} page={page} color="secondary" onChange={handleChange} />
+        </Stack>
+      </ThemeProvider>
+    </React.Fragment >
 
   )
 }
