c45f755f9ccb35bfe66243a2d9006e70c31ce181
springboot <nhokxuanquy@gmail.com>
2022-11-06 10:52:16 +0700

    updates

--- src/views/Home/BannerSlide.jsx
+++ src/views/Home/BannerSlide.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Slider from 'react-slick';
+import axios from "axios";
+import { API } from "../../utils/const";
 
 function BannerSlide() {
     var settings = {
@@ -11,29 +13,28 @@ function BannerSlide() {
         autoplay: true,
         autoplaySpeed: 2000,
         pauseOnHover: false,
-        slickPrev:false,
-        slickNext:false,
+        slickPrev: false,
+        slickNext: false,
         swipeToSlide: false,
     };
+    const [data, setData] = React.useState([]);
+    const getData = async () => {
+        const response = await axios.get(API + '/admin/webImage/?category=banner')
+        if (response.status === 200) {
+            setData(response.data)
+        }
+    }
+    useEffect(() => {
+        getData();
+    })
     return (
-        <div style={{ width: '1400px',borderRadius: '10px', margin:'auto' }}>
+        <div style={{ width: '1400px', borderRadius: '10px', height: '600px',margin: 'auto' }}>
             <Slider  {...settings}>
-                <div >
-                    {/* <h3 style={{color: 'white',textAlign: 'center'}}>1</h3> */}
-                    <img style={{ width: '100%', height: '600px', borderRadius: '10px' }} src="https://bizman.com.vn/storage/post/thumbnails/pano-quang-cao-b3c0c80a-45b6-4bd5-820a-54817f87dc72.jpg" alt="" />
-                </div>
-                <div>
-                    <img style={{ width: '100%', height: '600px', borderRadius: '10px' }} src="https://inbienquangcao.vn/wp-content/uploads/2020/01/Kha%CC%81i-nie%CC%A3%CC%82m-bie%CC%82%CC%89n-qua%CC%89ng-ca%CC%81o-pano-ta%CC%82%CC%81m-lo%CC%9B%CC%81n-ngoa%CC%80i-tro%CC%9B%CC%80i.jpg" alt="" />
-                </div>
-                <div>
-                    <img style={{ width: '100%', height: '600px', borderRadius: '10px' }} src="https://kynguyengroup.com/wp-content/uploads/2017/09/d26976b9d8e420ba79f5.jpg" alt="" />
-                </div>
-                <div>
-                    <img style={{ width: '100%', height: '600px', borderRadius: '10px' }} src="http://cdn-gd-v1.webbnc.net/useruploads/userfiles//506940/images/kich-thuoc-bien-quang-cao-ngoai-troi-3.jpg" alt="" />
-                </div>
-                <div>
-                    <img style={{ width: '100%', height: '600px', borderRadius: '10px' }} src="https://showroomdecor.com.vn/wp-content/uploads/2022/05/bien-quang-cao-dung-0.jpg" alt="" />
-                </div>
+                {data.map((i) => (
+                    <div >
+                        <img style={{ width: '100%', height: '600px', borderRadius: '10px' }} src={i.photosImagePath} alt="" />
+                    </div>
+                ))}
             </Slider>
         </div>
     );
