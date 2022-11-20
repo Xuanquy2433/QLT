24b144fdae3c9a8308a812189b06ed37e8be05b1
springboot <nhokxuanquy@gmail.com>
2022-11-20 16:41:41 +0700

    u

--- src/views/pillarAddress/AddressPointBox.jsx
+++ src/views/pillarAddress/AddressPointBox.jsx
@@ -56,10 +56,10 @@ const columnsDetail = [
 ];
 const style = {
   position: 'absolute',
-  top: '45%',
+  top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 1200,
+  width: 1300,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
