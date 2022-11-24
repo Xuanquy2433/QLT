7f88846917d2720b66739f1deb6fe7af682b68ac
springboot <nhokxuanquy@gmail.com>
2022-11-24 16:39:44 +0700

    fix cart

--- src/views/examples/Login.js
+++ src/views/examples/Login.js
@@ -64,21 +64,22 @@ const Login = () => {
           toast.success('Đăng nhập thành công', {
             autoClose: 1500
           })
-          if (localStorage.getItem('countCart') == null) {
-            const response2 = await axios.get(API_GET_CART, {
-              headers: {
-                'authorization': 'Bearer ' + response?.data.token,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              }
-            })
-            if (response) {
-              localStorage.setItem('countCart', JSON.stringify(response2.data.length));
-              window.dispatchEvent(new Event("storage"));
-            }
-          }
+          
           // add cart local to database
           if (jwt_decode(response?.data.token).roles === `[ROLE_USER]`) {
+            if (localStorage.getItem('countCart') == null) {
+              const response2 = await axios.get(API_GET_CART, {
+                headers: {
+                  'authorization': 'Bearer ' + response?.data.token,
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                }
+              })
+              if (response) {
+                localStorage.setItem('countCart', JSON.stringify(response2.data.length));
+                window.dispatchEvent(new Event("storage"));
+              }
+            }
             history.goBack()
             //add cart local to database
             var myMap = new Map()
