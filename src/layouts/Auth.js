2dcb62c4b5e658d8efc34837c756fc4b09bf09e7
Trung Quý <letrungquy50@gmail.com>
2022-11-26 10:16:40 +0700

    update-navbar

--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -70,24 +70,7 @@ const Auth = (props) => {
     <>
       <div className="main-content" ref={mainContent}  >
         <AuthNavbar />
-        <div className="header py-7 py-lg-7 mt-4">
-          {/* bg-gradient-info */}
-          {/* <div className="separator separator-bottom separator-skew zindex-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="fill-default"
-                points="2560 0 2560 100 0 100"
-              />
-            </svg>
-          </div> */}
-        </div>
+        
 
         <Container className="mt--8 pb-5">
           <Row className="justify-content-center">
--- src/views/Home/BannerSlide.jsx
+++ src/views/Home/BannerSlide.jsx
@@ -29,11 +29,11 @@ function BannerSlide() {
         getData();
     }, [])
     return (
-        <div style={{ width: '1480px', margin: 'auto' }}>
+        <div style={{ width: '1480px', margin: 'auto' , marginTop:'8rem' }}>
             <Slider  {...settings}>
                 {data.map((i) => (
                     <div >
-                        <img style={{ width: '100%', height: '85vh', overflowX:"hidden", margin:"auto !important" }} src={i.photosImagePath} alt="" />
+                        <img style={{ width: '100%', height: '100vh', overflowX:"hidden", margin:"auto !important" }} src={i.photosImagePath} alt="" />
                     </div>
                 ))}
             </Slider>
