011ea24d34e44998c2bc94c0b236c20113bf3fab
Bakabot307 <60350866+Bakabot307@users.noreply.github.com>
2022-12-09 15:40:41 +0700

    add validate for bank

--- src/views/pillarAddress/ProductComponent.jsx
+++ src/views/pillarAddress/ProductComponent.jsx
@@ -302,10 +302,11 @@ function ProductComponent({ product, onClickRemoveItemCart, addCart, setItem }) 
                         fontWeight: "500", width: "100%"
                         , border: "3px solid #333", background: "none", color: "#FFFFFF", boxShadow: "none", backgroundColor: '#333'
                       }} disabled variant="contained" >
-                        Đã cho thuê
+                        {item.status === 'HIRING' && item.expiredDate !== null ? <div style={{ height: "28.5px" }}> <h4 >
+                         <span style={{ color: 'red' }}> <Moment format="DD/MM/YYYY">{item.expiredDate}</Moment></span> </h4> </div> : ''}
                       </Button>
                   }
-                  {item.status === 'HIRING' && item.expiredDate !== null ? <div style={{ height: "28.5px" }}> <h4 style={{ marginTop: '15px', }}> Ngày hết hạn: <span style={{ color: 'red' }}> <Moment format="DD/MM/YYYY">{item.expiredDate}</Moment></span> </h4> </div> : ''}
+
                 </div>
                 {/* {
                   item.preOrdered === false ?
